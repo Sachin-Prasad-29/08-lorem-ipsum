46200ab240b2abe1e8b197c53ac3373ada70f043
Sachin <skpkorba9009@gmail.com>
2022-10-10 15:17:39 +0530

    done and dusted

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import text from './data';
+import data from './data';
+function App() {
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState([]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let amount = parseInt(count);
+        if (count <= 0) {
+            amount = 1;
+        }
+        if (count > 8) {
+            amount = 8;
+        }
+        setText(data.slice(0, amount));
+    };
+    return (
+        <section className='section-center'>
+            <h3>tired of Boring Lorem inpsum ?</h3>
+            <form className='lorem-form' onSubmit={handleSubmit}>
+                <label htmlFor='amount'>Paragraphs:</label>
+                <input
+                    type='number'
+                    name='amount'
+                    id='amount'
+                    value={count}
+                    onChange={(e) => setCount(e.target.value)}
+                />
+                <button type='submit' className='btn'>
+                    Generate
+                </button>
+            </form>
+            <article className='lorem-text'>
+                {text.map((para, index) => {
+                    return <p key={index}>{para}</p>;
+                })}
+            </article>
+        </section>
+    );
+}
+
+export default App;
